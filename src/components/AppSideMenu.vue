1c9d18fc256d99cb3e4b9984bcb3d28b585a02b5
manoliszi <manolis.zifos@gmail.com>
2025-03-29 13:54:32 +0200

    clean up code from unnecessary stuff

--- src/components/AppSideMenu.vue
+++ src/components/AppSideMenu.vue
@@ -1,27 +1,27 @@
 <template>
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list>
-        <v-list-item title="My Jokes Application"></v-list-item>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-list>
+      <v-list-item title="My Jokes Application"></v-list-item>
 
-        <v-divider></v-divider>
+      <v-divider></v-divider>
 
-        <v-list-item 
-          v-for="item in menuItems" 
-          :key="item.title" 
-          :to="item.route"
-          :prepend-icon="item.icon"
-          color="primary"
-          link
-        >
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+      <v-list-item 
+        v-for="item in menuItems" 
+        :key="item.title" 
+        :to="item.route"
+        :prepend-icon="item.icon"
+        color="primary"
+        link
+      >
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 
-    <v-app-bar app color="secondary">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>My Jokes Application</v-toolbar-title>
-    </v-app-bar>
+  <v-app-bar app color="secondary">
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title>My Jokes Application</v-toolbar-title>
+  </v-app-bar>
 </template>
 
 <script setup>
